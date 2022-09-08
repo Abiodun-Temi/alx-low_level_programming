22ba43361736271d6b9daa735de99cb1ca4fb2f4
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-09-09 00:05:51 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+include <stdio.h>
+
 /**
-* main - Entry point
+* main -  The entry point of this program
 *
-* Return: Always 0 (Success)
+* Description: This is meant to tell the user something
+*
+* Return: 0 everytime
 */
 int main(void)
 {
-
-     printf(" with proper grammar, but the outcome is a piece of art,\n");
-     return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
