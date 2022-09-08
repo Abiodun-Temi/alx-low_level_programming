52a1efd7c36c28fdb43cbf8c943b852919a42128
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-09-09 00:34:42 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - This is my function main.
+ * main - This is my function/method main.
  *
- * Description: Print out a message to the user.
+ * Description: Print out a message to the user at the moment.
  *
- * Return: 0 everytime. So it is successful.
+ * Return: (0) success
  */
 int main(void)
 {
