340393832a2a993b39aabfd6447b726cf1a96636
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-09 10:27:58 -0700

    file error cleared

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	  printf("%i is positive");
+	  printf("%i is positive", n);
 	else if (n < 0)
-	  printf("%i is negative");
+	  printf("%i is negative", n);
 	else
-	  printf("%i is zero \n");
+	  printf("%i is zero \n", n);
 	return (0);
 }
