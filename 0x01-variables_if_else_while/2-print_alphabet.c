7379c85e6d611a0fedb3fdab7ddce1abefbe4c4f
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-09 14:47:05 -0700

    c file to list alphabet a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*
+*
+*Return: End the program
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z', ++alphabet)
+	printf("%c\n", alphabet);
+
+	return (0);
+}
+	
