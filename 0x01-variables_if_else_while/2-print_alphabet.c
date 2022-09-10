04b0bf540d54aa6cd47a09c79e09d0230d2e39f8
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-09-10 01:25:41 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z', ++alphabet)
+	for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
 	printf("%c\n", alphabet);
 	return (0);
 }
