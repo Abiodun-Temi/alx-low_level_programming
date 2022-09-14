41b8e6f011e293709cfc797df53ff08ec26194dd
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-13 20:11:35 -0700

    file updated

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
  * _abs - prints absolute value of an interger
- *@int: test for absolute value
+ *@n: test for absolute value
  *Return: return absolute value of a number
  */
-int _abs(int)
+int _abs(int n)
 {
 	if (n < 0)
 		n = -n;
 	return (n);
-
-
 }
