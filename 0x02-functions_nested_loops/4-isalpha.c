93f94c087eaade4014f7c169403e0201a562a1a1
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-13 19:19:49 -0700

    file added for both upper and lower case letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is in lower case or upper case
+ *@c: checks input of method
+ * Return: returns 1 if 'c' is in lowercase or uppercase else return 0
+*/
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c >= 'Z');
+}
