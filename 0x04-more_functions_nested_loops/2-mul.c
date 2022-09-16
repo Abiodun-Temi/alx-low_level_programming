280bd0b909793d7334dfa5200fe0339b4b6a2332
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-15 18:02:27 -0700

    2 multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - name function to multiply 2 intergers
+ * @a: one of the variable to store value for multiplication
+ * @b: second variable to store second num for multiplication
+ * Return: return a multipied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
