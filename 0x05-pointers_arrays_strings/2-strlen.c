6d72af405c09c7364d78f7429e472903248c459d
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-19 19:11:19 -0700

    lenght of stirng gotten

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function to get the length of a string
+ * @s - length of stirng is stored
+ * Return: return length of string
+ */
+int _strlen(char *s)
+{
+	int lent;
+	for(; *s != '\0'; s++)
+		len +=1;
+
+	return (len);
+}
