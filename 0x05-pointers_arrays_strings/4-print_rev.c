2c521f7f75eb1d6f9bb0ecc4f7f4da65c20492b6
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-19 20:16:25 -0700

    reverse string count code added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: print reverse variable
+ * Return: return nothing
+ */
+void print_rev(char *s)
+{
+	int revcount = 0;
+
+	while (revcount >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		revcount++;
+	}
+	for (revcount--; revcount >= 0; revcount--)
+	{
+		_putchar(s[revcount]);
+	}
+	_putchar('\n');
+}
