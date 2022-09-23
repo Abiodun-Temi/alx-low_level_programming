6aa849e44b66e472733a89f0d64db22c524f4456
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-22 18:43:48 -0700

    strcmp function created

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - function to compare 2 strings
+ * @s1: first character array or string
+ * @s2: second character array or string
+ * Return: return result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+
+}
