ce2a1c11859e062b1ecfbfbf0961910aa6e0fb6b
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-26 18:32:09 -0700

    file added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - this is a memory set function
+ * @s: string
+ * @b: character variable
+ * @n: an interger variable
+ * Return: return string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int t;
+
+	for (t = 0; t < n; t++)
+		s[t] = b;
+
+	return (s);
+}
