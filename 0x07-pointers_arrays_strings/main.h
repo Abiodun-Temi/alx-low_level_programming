a46b5153429124c5b7aba668f859c20115b3b3d6
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-09-27 03:13:58 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,10 +3,9 @@
 
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
-<<<<<<< HEAD
 char *_memcpy(char *dest, char *src, unsigned int n);
-=======
+
 
 
 #endif
->>>>>>> cf322dc32d7ab19abd6e6616ac8bd65e24c1b256
+
