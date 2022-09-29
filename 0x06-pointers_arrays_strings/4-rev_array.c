69190ec4dccfc0ed01447354eec67af8567091df
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-09-29 02:54:30 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,29 +1,27 @@
 /**
  * reverse_array - A function that reverses the order of the elements
  *					in an array of integers.
- *
  * @a: The array of integers to sort.
  * @n: The number of elements in the array to reverse.
- *
  * Return: Does not return a value.
 */
 
 void reverse_array(int *a, int n)
 {
-	int temp, s, e;
+	int temp, t, e;
 
-	s = 0;
+	t = 0;
 	e = n - 1; /* The index of the final element to sort. */
 
 	/* While the ofrward index has not equalised with the backward index */
-	while (s < e)
+	while (t < e)
 	{
-		temp = a[s];
+		temp = a[t];
 
-		/* Swap the elements at the offset s and e */
-		a[s] = a[e];
+		/* Swap the elements at the offset t and e */
+		a[t] = a[e];
 		a[e] = temp;
-		s++; /* Shift the forward index forward */
+		t++; /* Shift the forward index forward */
 		e--; /* Shift the backward index backward */
 	}
 }
