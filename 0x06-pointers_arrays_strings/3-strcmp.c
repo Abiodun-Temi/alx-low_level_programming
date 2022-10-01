9c9e74b70259e74c01c258d4fa3899322a5be4fe
Abiodun Temiloluwa <111081387+Abiodun-Temi@users.noreply.github.com>
2022-10-01 04:46:49 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        int result;
+int i = 0, op = 0;
 
-        result = strcmp(s1, s2);
-        return (result);
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
 
+	return (op);
 }
 
