23c34e97778586d11f4d76dab1391a518257aad7
Abiodun Temiloluwa <tabiodun19@gmail.com>
2022-09-30 19:25:00 -0700

    program using arguements

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive intergers
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int t, e, add = 0;
+
+	for (t = 1; t< argc; t++)
+	{
+		for (t = 0; argv[t][e] != '\0'; e++)
+		{
+			if (!isdigit(argv[t][e]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[t]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
